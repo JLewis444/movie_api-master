07957f0934414aa274a69c2245f22eb3077d1bb5
Olaoluwa Lewis <olipompin@gmail.com>
2019-09-12 20:23:34 +0200

    rendered

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { MainView} from './component/main-view/main-view';
+
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
 // Main component 
 class MyFlixApplication extends React.Component {
     render() {
-        return ( 
-            <div className="my-flix">
-                <div>Good Morning</div>
-            </div>
-        );
+        return <MainView/>;
     }
 }
 
